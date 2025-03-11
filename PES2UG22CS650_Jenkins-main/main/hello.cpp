a9db248f9a8451b65c01219ebb79b195ab831465
PES2UG22CS650 <144761953+varun8861@users.noreply.github.com>
2025-03-11 19:39:32 +0530

    Update hello.cpp

--- PES2UG22CS650_Jenkins-main/main/hello.cpp
+++ PES2UG22CS650_Jenkins-main/main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hellooooooooo, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
